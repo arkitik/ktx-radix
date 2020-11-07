6e538662041fbd4b59676e7a59f0fce5f54beb94
iloom <ibm.iloom@gmail.com>
2020-11-07 14:12:06 +0200

    feat: Add usecase request and response adapter. #2

---
+++ ktx-radix-development/ktx-radix-development-usecase-adapter/src/main/java/io/quee/ktx/radix/develop/usecase/adapter/usecase-adapter.ext.kt
@@ -0,0 +1,44 @@
+package io.quee.ktx.radix.develop.usecase.adapter
+
+import io.quee.ktx.radix.develop.usecase.*
+import io.quee.ktx.radix.develop.usecase.factory.UseCaseFactory
+import io.quee.ktx.radix.develop.usecase.model.UseCaseRequest
+import io.quee.ktx.radix.develop.usecase.model.UseCaseResponse
+
+/**
+ * Created By [*Ibrahim AlTamimi *](https://www.linkedin.com/in/iloom/)
+ * Created At 07, **Sat Nov, 2020**
+ * Project **ktx-radix** [Quee.IO](https://quee.io)
+ */
+infix fun <RQ, RS> FunctionalUseCase<RequestAdapter<RQ>, ResponseAdapter<RS>>.adapterProcess(request: RQ) =
+        (this process RequestAdapter(request)).response
+
+infix fun <RQ, RS : UseCaseResponse> FunctionalUseCase<RequestAdapter<RQ>, RS>.adapterProcess(request: RQ) =
+        this process RequestAdapter(request)
+
+infix fun <RQ : UseCaseRequest, RS> FunctionalUseCase<RQ, ResponseAdapter<RS>>.adapterProcess(request: RQ) =
+        (this process request).response
+
+infix fun <RQ, RS> FunctionalUseCase<RequestAdapter<RQ>, ResponseAdapter<RS>>.adapterProcess(requestProvider: () -> RQ) =
+        this adapterProcess requestProvider()
+
+infix fun <RQ, RS : UseCaseResponse> FunctionalUseCase<RequestAdapter<RQ>, RS>.adapterProcess(request: () -> RQ) =
+        this adapterProcess request()
+
+infix fun <RQ> CommandUseCase<RequestAdapter<RQ>>.adapterExecute(request: RQ) =
+        this execute RequestAdapter(request)
+
+infix fun <RQ> CommandUseCase<RequestAdapter<RQ>>.adapterExecute(requestProvider: () -> RQ) =
+        this adapterExecute requestProvider()
+
+infix fun <F : UseCaseFactory, RQ, RS> F.adapterFunctional(functionalUseCase: F.() -> FunctionalUseCase<RequestAdapter<RQ>, ResponseAdapter<RS>>) =
+        this functional functionalUseCase
+
+infix fun <F : UseCaseFactory, RQ : UseCaseRequest, RS> F.adapterFunctionalReq(functionalUseCase: F.() -> FunctionalUseCase<RQ, ResponseAdapter<RS>>) =
+        this functional functionalUseCase
+
+infix fun <F : UseCaseFactory, RQ, RS : UseCaseResponse> F.adapterFunctionalResp(functionalUseCase: F.() -> FunctionalUseCase<RequestAdapter<RQ>, RS>) =
+        this functional functionalUseCase
+
+infix fun <F : UseCaseFactory, RQ> F.adapterCommand(commandUseCase: F.() -> CommandUseCase<RequestAdapter<RQ>>) =
+        this command commandUseCase
